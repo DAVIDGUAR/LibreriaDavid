2772ef6b13df013cc081e1c0f424d046a47ba893
drodriguezguardiola <drodriguezguardiola@nautilus19.danielcastelao.org>
2019-01-17 10:28:35 +0100

    cambios david

--- src/libreriapropia/IO.java
+++ src/libreriapropia/IO.java
@@ -1,33 +1,35 @@
 package libreriapropia;
 
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author drodriguezguardiola
  */
 public class IO {
-    
-    int numero;
-    
-    
-   
-    
-  final public int Introducir(int numero,String mensaje){               
 
-        
+    
+
+    final public int Introducir(int numero, String mensaje) {
+
         System.out.println("1-> consola\n 2-> ventana");
-    
-            Scanner ler = new Scanner (System.in);
-            int num = ler.nextInt();
-            
-            switch(numero){
-            case 1: System.out.println("consola");
-            break;
-            case 2: System.out.println("ventana");
-            break;
-            default:System.out.println("opcion non valida");
-}
-}
-}
+        int resultado = 0;
 
+        switch (numero) {
+            case 1:
+                System.out.println("consola");
+                Scanner ler = new Scanner(System.in);
+                int num = ler.nextInt();
+                String aux = JOptionPane.showInputDialog(mensaje);
+                resultado = Integer.parseInt(aux);
+                break;
+            case 2:
+                System.out.println("ventana");
+                break;
+            default:
+                System.out.println("opcion non valida");
+        }
+        return resultado;
+    }
+}
